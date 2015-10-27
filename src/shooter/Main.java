a37c9cd73376b9c99ebbe2369e50a9d6cb2d6954
Quadx117 <ericperron@videotron.ca>
2015-10-27 17:47:14 -0400

    Add/Remove TODO's
    - Remove completed TODO's
    - Add new TODO's

--- src/shooter/Main.java
+++ src/shooter/Main.java
@@ -15,6 +15,9 @@ public class Main
 	}
 }
 
-// TODO: Inputs
+// TODO: Mouse Inputs
 // TODO: Sound
+// TODO: Finish validating Java keycode vs .NET (Keys.java)
+// TODO: add Font anti-aliasing to software renderer ?
+// TODO: frame doesn't have keyboard focus at start up
 // TODO: Should probably create a separate git repo for the Shooter game code.
