48de7fd2ad75f3c45ef243b513dd859fd9db9d32
Quadx117 <ericperron@videotron.ca>
2015-11-19 17:34:02 -0500

    Add comments and missing method
    - Add comments to methods
    - Add missing method (getLocation)

--- src/gameCore/input/MouseEventArgs.java
+++ src/gameCore/input/MouseEventArgs.java
@@ -1,7 +1,14 @@
 package gameCore.input;
 
+import gameCore.Point;
 import gameCore.dotNet.events.EventArgs;
 
+/**
+ * Provides data for the {@code MouseUp}, {@code MouseDown}, and {@code MouseMove} events.
+ * 
+ * @author Eric Perron
+ *
+ */
 public class MouseEventArgs extends EventArgs
 {
 	private MouseButtons button;
@@ -33,26 +40,62 @@ public class MouseEventArgs extends EventArgs
 		this.delta = delta;
 	}
 
+	/**
+	 * Gets which mouse button was pressed.
+	 * 
+	 * @return The mouse button that was pressed.
+	 */
 	public MouseButtons getButton()
 	{
 		return button;
 	}
 
+	/**
+	 * Gets the number of times the mouse button was pressed and released.
+	 * 
+	 * @return The number of times the mouse button was pressed and released.
+	 */
 	public int getClicks()
 	{
 		return clicks;
 	}
 
+	/**
+	 * Gets the x-coordinate of the mouse during the generating mouse event.
+	 * 
+	 * @return The x-coordinate of the mouse during the generating mouse event.
+	 */
 	public int getX()
 	{
 		return x;
 	}
 
+	/**
+	 * Gets the y-coordinate of the mouse during the generating mouse event.
+	 * 
+	 * @return The y-coordinate of the mouse during the generating mouse event.
+	 */
 	public int getY()
 	{
 		return y;
 	}
 
+	/**
+	 * Gets the location of the mouse during the generating mouse event.
+	 * 
+	 * @return The location of the mouse during the generating mouse event.
+	 */
+	public Point getLocation()
+	{
+		return new Point(x, y);
+	}
+
+	/**
+	 * Gets a signed count of the number of detents the mouse wheel has rotated, multiplied by the
+	 * WHEEL_DELTA constant. A detent is one notch of the mouse wheel.
+	 * 
+	 * @return The number of detents the mouse wheel has rotated.
+	 */
 	public int getDelta()
 	{
 		return delta;
